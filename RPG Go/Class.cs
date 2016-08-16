c4b0a90a414f9c5457b33f2c75248e3630a389bb
John <John@JOHN-PC>
2016-08-16 13:57:34 -0400

    Add project files.

---
+++ RPG Go/Class.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RPG_Go
+{
+
+    namespace PlayersHandbook
+    {
+        using System.Collections;
+
+        public abstract class Class
+        {
+            private string name; // 'class name'
+            private string description; // 'class description'
+            private string size; // 'medium'
+            private int speed; // 30
+            private int startingAge; // 20
+            private ArrayList maleNames;
+            private ArrayList femaleNames;
+            private ArrayList surnames;
+            private ArrayList languages;
+            private ArrayList proficiencies;
+
+            // Properties
+            public abstract string Name { get; }
+            public abstract string Description { get; }
+            public abstract string Size { get; }
+            public abstract int Speed { get; }
+            public abstract int StartingAge { get; }
+            public abstract ArrayList MaleNames { get; }
+            public abstract ArrayList FemaleNames { get; }
+            public abstract ArrayList Surnames { get; }
+            public abstract ArrayList Languages { get; }
+            public abstract ArrayList Proficiencies { get; }
+
+        }
+    }
+}
---
+++ RPG Go/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RPG_Go.PlayersHandbook;
+namespace RPG_Go
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> cw = Console.WriteLine;
+            bool done = false;
+            while (!done)
+            {
+                
+                cw("Butts");
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "d":
+                        Dwarf D = new Dwarf();
+                        cw(D.Description);
+                        break;
+                    case "q":
+                        done = true;
+                        break;
+                    default:
+                        cw("what?");
+                        break;
+
+                }
+            }
+            
+        }
+
+        void DoIt()
+        {
+
+
+        }
+    }
+}
